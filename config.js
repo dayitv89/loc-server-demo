4f2658ba33667fef4ad0662d61cbde4bb49fc5ca
Vikram Biwal <crazyvk56@gmail.com>
2017-12-22 15:20:22 +0530

    Initial commit

---
+++ config.js
@@ -0,0 +1,12 @@
+export const facebook = {
+	clientID: '1593375307393848',
+	clientSecret: 'f8df237c205728efa13a19973f7c90f3',
+	callbackURL: 'http://10.1.1.13:3000/auth/facebook/callback',
+	profileFields: ['id', 'name', 'displayName', 'picture', 'email']
+};
+
+export const google = {
+	clientID: 'INSERT-CLIENT-ID-HERE',
+	clientSecret: 'INSERT-CLIENT-SECRET-HERE',
+	callbackURL: 'http://localhost:3000/auth/google/callback'
+};
